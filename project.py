7db573781608d62dfcf2fa78e8f88a3766d3df18
Lee <amecac21@gmail.com>
2023-04-01 01:19:06 -0400

    code structure

--- project.py
+++ project.py
@@ -1,10 +1,14 @@
 def main():
-    name = input("What's your name? ")
-    if len(name) > 1:
-        first, last = name.split()
-        print(f"hello, {first}")
-    else:
-        print(f"hello, {name}")
+    ...
+
+def func1():
+    ...
+
+def func2():
+    ...
+
+def func3():
+    ...
 
 if __name__ == "__main__":
     main()
--- test_project.py
+++ test_project.py
@@ -1,2 +1,10 @@
 import project
 
+def test_func1():
+    ...
+
+def test_func2():
+    ...
+
+def test_func3():
+    ...
