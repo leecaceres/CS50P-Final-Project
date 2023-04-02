4b9ebb37a2ecf5a802aff770ae92693627451051
Lee <amecac21@gmail.com>
2023-04-01 21:32:11 -0400

    Implemented Get Sign Function, works for mm/dd

--- project.py
+++ project.py
@@ -24,7 +24,7 @@ def get_DOB():
     s = input("What is your birthday? ").strip()
 
     # mm/dd/yy or mm/dd/yyyy
-    if matches := re.search(r"^(\d{2})/(\d{2})/(\d{2}|\d{4})$", s):
+    if matches := re.search(r"^(\d{1,2})/(\d{1,2})(/\d{2}|\d{4})?$", s):
         month = int(matches.group(1))
         day = int(matches.group(2))
 
@@ -44,16 +44,48 @@ def get_DOB():
             
     # month dd, yyyy
     if matches := re.search(r"(.+) (\d{1,2}),? (\d{4})", s, re.IGNORECASE):
-        if months[matches.group(1)] in months:
-            print()
-
+        month = int(matches.group(1))
+        day = int(matches.group(2))
 
     # dd month, yyyy
     # if matches := re.search(r"(\d{1,2}) (.+),? (\d{4})", s, re.IGNORECASE):
         # ...
 
 def get_sign(month, day):
-    return "taurus"
+    signs = [
+        {"name": "Aries", "range": [{"month": "March", "dates": range(21, 32)},
+                                    {"month": "April", "dates": range(1, 20)},]},
+        {"name": "Taurus", "range": [{"month": "April", "dates": range(20, 31)},
+                                    {"month": "May", "dates": range(1, 21)},]},
+        {"name": "Gemini", "range": [{"month": "May", "dates": range(21, 32)},
+                                    {"month": "June", "dates": range(1, 21)},]},
+        {"name": "Cancer", "range": [{"month": "June", "dates": range(21, 31)},
+                                    {"month": "July", "dates": range(1, 23)},]},
+        {"name": "Leo", "range": [{"month": "July", "dates": range(23, 32)},
+                                    {"month": "August", "dates": range(1, 23)},]},
+        {"name": "Virgo", "range": [{"month": "August", "dates": range(23, 32)},
+                                    {"month": "September", "dates": range(1, 23)},]},
+        {"name": "Libra", "range": [{"month": "September", "dates": range(23, 31)},
+                                    {"month": "October", "dates": range(1, 23)},]},
+        {"name": "Scorpio", "range": [{"month": "October", "dates": range(23, 32)},
+                                    {"month": "November", "dates": range(1, 22)},]},
+        {"name": "Sagittarius", "range": [{"month": "November", "dates": range(23, 32)},
+                                    {"month": "December", "dates": range(1, 22)},]},
+        {"name": "Capricorn", "range": [{"month": "December", "dates": range(22, 32)},
+                                    {"month": "January", "dates": range(1, 20)},]},
+        {"name": "Aquarius", "range": [{"month": "January", "dates": range(20, 30)},
+                                    {"month": "February", "dates": range(1, 19)},]},
+        {"name": "Pisces", "range": [{"month": "February", "dates": range(19, 32)},
+                                    {"month": "March", "dates": range(1, 21)},]},
+    ]
+
+    for sign in signs:
+        for m in sign["range"]:
+            if m["month"] == month:
+                if day in m["dates"]:
+                    return sign["name"]
+
+    raise ValueError("DOB/Sign not matched")
 
 # def get_horoscope():
 #     ...
