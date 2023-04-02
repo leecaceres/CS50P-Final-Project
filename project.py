9af0df6d3188f64a0db121bb075cd3eda2513cfe
Lee <amecac21@gmail.com>
2023-04-01 23:08:40 -0400

    Added short as key in months dict

--- project.py
+++ project.py
@@ -1,25 +1,13 @@
+from curses.ascii import isdigit
+# from pip._vendor import requests
 import re
 
 def main():
     month, day = get_DOB()
     print(get_sign(month, day))
+    # get_horoscope(get_sign(month, day))
 
 def get_DOB():
-    
-    months = [
-        {"month": "January", "number": 1, "length": 31},
-        {"month": "February", "number": 2, "length": 29},
-        {"month": "March", "number": 3, "length": 31},
-        {"month": "April", "number": 4, "length": 30},
-        {"month": "May", "number": 5, "length": 31},
-        {"month": "June", "number": 6, "length": 30},
-        {"month": "July", "number": 7, "length": 31},
-        {"month": "August", "number": 8, "length": 31},
-        {"month": "September", "number": 9, "length": 30},
-        {"month": "October", "number": 10, "length": 31},
-        {"month": "November", "number": 11, "length": 30},
-        {"month": "December", "number": 12, "length": 31},
-    ]
 
     s = input("What is your birthday? ").strip()
 
@@ -28,30 +16,69 @@ def get_DOB():
         month = int(matches.group(1))
         day = int(matches.group(2))
 
-        # Check if day is valid
-        if day <= 0 or day > 31:
-                raise ValueError("Invalid date")
-        
-        for m in months:         
-            # Check if date in month length
-            if m["number"] == month:
-                if day > m["length"]:
-                    raise ValueError
-                month = m["month"]
-                return(month, day)
-            
-        raise ValueError("Invalid date")
+        return check_DOB(month, day)
             
     # month dd, yyyy
-    if matches := re.search(r"(.+) (\d{1,2}),? (\d{4})", s, re.IGNORECASE):
-        month = int(matches.group(1))
+    if matches := re.search(r"^(\w+) (\d{1,2})(,? \d{4})?$", s, re.IGNORECASE):
+        month = matches.group(1).title()
         day = int(matches.group(2))
 
+        return check_DOB(month, day)
+
     # dd month, yyyy
-    # if matches := re.search(r"(\d{1,2}) (.+),? (\d{4})", s, re.IGNORECASE):
-        # ...
+    if matches := re.search(r"^(\d{1,2}) (\w+)(,? \d{4})?$", s, re.IGNORECASE):
+        month = matches.group(2).title()
+        day = int(matches.group(1))
+
+        return check_DOB(month, day)
+
+def check_DOB(month, day):
+    months = [
+        {"month": "January", "number": 1, "length": 31, "short": "Jan"},
+        {"month": "February", "number": 2, "length": 29, "short": "Feb"},
+        {"month": "March", "number": 3, "length": 31, "short": "Mar"},
+        {"month": "April", "number": 4, "length": 30, "short": "Apr"},
+        {"month": "May", "number": 5, "length": 31, "short": None},
+        {"month": "June", "number": 6, "length": 30, "short": "Jun"},
+        {"month": "July", "number": 7, "length": 31, "short": "Jul"},
+        {"month": "August", "number": 8, "length": 31, "short": "Aug"},
+        {"month": "September", "number": 9, "length": 30, "short": "Sep"},
+        {"month": "October", "number": 10, "length": 31, "short": "Oct"},
+        {"month": "November", "number": 11, "length": 30, "short": "Nov"},
+        {"month": "December", "number": 12, "length": 31, "short": "Dec"},
+    ]
+
+    # Check if day is valid
+    if day <= 0 or day > 31:
+        raise ValueError("Invalid day")
+    
+    
+    for m in months:
+        # If month is int, find name
+        if isinstance(month, int):
+            if month <=0 or month > 12:
+                raise ValueError("Invalid month")
+            if m["number"] == month:
+                month = m["month"]
+
+        # If shortened month name used, switch to full
+        if m["short"] == month:
+            month = m["month"]
+        
+        # Check if month name + date are valid
+        if m["month"] == month:
+            # Check day in length
+            if day > m["length"]:
+                raise ValueError("Invalid day")
+            
+            return(month, day)
+                
+    raise ValueError("Invalid")
+
 
 def get_sign(month, day):
+
+    # Sun Signs and corresponding date ranges
     signs = [
         {"name": "Aries", "range": [{"month": "March", "dates": range(21, 32)},
                                     {"month": "April", "dates": range(1, 20)},]},
@@ -87,8 +114,8 @@ def get_sign(month, day):
 
     raise ValueError("DOB/Sign not matched")
 
-# def get_horoscope():
-#     ...
+def get_horoscope(sign):
+    ...
 
 if __name__ == "__main__":
     main()
